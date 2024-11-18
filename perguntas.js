c6add5c26d57577047363a3cf136e15ee3d7388a
Igor Lima <igorlego86@gmail.com>
2024-11-18 08:00:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma div em HTML?',
+    'Uma div é uma tag que separa seções da marcação'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é Bye (BAI)'
+)
